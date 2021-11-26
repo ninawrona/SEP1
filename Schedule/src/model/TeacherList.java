74232bd47dafa8e8efb5e3d13df9ae7145415fb3
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2021-11-26 14:19:29 +0100

    techaerListVerifivation

--- Schedule/src/model/TeacherList.java
+++ Schedule/src/model/TeacherList.java
@@ -6,25 +6,36 @@ public class TeacherList
 {
   private ArrayList<Teacher> teachers;
 
-  public TeacherList(){
+  public TeacherList()
+  {
     teachers = new ArrayList<>();
   }
 
-  public int size(){
+  public int size()
+  {
     return teachers.size();
   }
 
-  public void addTeacher(Teacher teacher){
+  public void addTeacher(Teacher teacher)
+  {
     teachers.add(teacher);
   }
 
-  public void removeTeacher(Teacher teacher){
+  public void removeTeacher(Teacher teacher)
+  {
     teachers.remove(teacher);
   }
 
-  public Teacher getTeacherByName(String name){
-    for (int i = 0; i < teachers.size(); i++){
-      if (teachers.get(i).getName().equals(name)){
+  public Teacher getTeacherByName(String name)
+  {
+    if(name == null)
+    {
+      throw new IllegalArgumentException("Name cannot be null!");
+    }
+    for (int i = 0; i < teachers.size(); i++)
+    {
+      if (teachers.get(i).getName().equals(name))
+      {
         return teachers.get(i);
       }
     }
@@ -32,9 +43,14 @@ public class TeacherList
     return null;
   }
 
-  public Teacher getTeacherByViaId(String viaId){
-    for (int i = 0; i < teachers.size();i++){
-      if (teachers.get(i).getViaId().equals(viaId)){
+  public Teacher getTeacherByViaId(String viaId)
+  {
+    //There might be an exception if there more than 4 characters or so
+    //but we are not sure
+    for (int i = 0; i < teachers.size(); i++)
+    {
+      if (teachers.get(i).getViaId().equals(viaId))
+      {
         return teachers.get(i);
       }
     }
@@ -42,29 +58,36 @@ public class TeacherList
     return null;
   }
 
-  public Teacher getTeacherByIndex(int index){
+  public Teacher get(int index)
+  {
     return teachers.get(index);
   }
 
-  public String toString(){
+  public String toString()
+  {
     String str = "";
-    for (int i = 0; i < teachers.size(); i++){
+    for (int i = 0; i < teachers.size(); i++)
+    {
       str += teachers.get(i).toString();
     }
     return str;
   }
 
-  public boolean equals(Object obj){
-    if (!(obj instanceof TeacherList)){
+  public boolean equals(Object obj)
+  {
+    if (!(obj instanceof TeacherList))
+    {
       return false;
     }
 
-    TeacherList other = (TeacherList)obj;
+    TeacherList other = (TeacherList) obj;
 
     boolean equals = true;
 
-    for (int i = 0; i < teachers.size(); i++){
-      if (!(teachers.get(i).equals(other.teachers.get(i)))){
+    for (int i = 0; i < teachers.size(); i++)
+    {
+      if (!(teachers.get(i).equals(other.get(i))))
+      {
         equals = false;
       }
     }

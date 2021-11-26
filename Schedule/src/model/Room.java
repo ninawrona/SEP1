f3c0a48564e361d805fb571e8566eb4ee5452a23
MartyBobo <martin.rosendahl4@gmail.com>
2021-11-26 13:32:18 +0100

    made room class

--- Schedule/src/model/Room.java
+++ Schedule/src/model/Room.java
@@ -9,6 +9,38 @@ public class Room
   private boolean available;
 
   public Room(int floor, char block, int number, int capacity){
+    this.floor = floor;
+    this.block = block;
+    this.number = number;
+    this.capacity = capacity;
+    available = true;
+  }
 
+  public void book(){
+    available = false;
+  }
+
+  public boolean isAvailable(){
+    return available;
+  }
+
+  public int getFloor(){
+    return floor;
+  }
+
+  public char getBlock(){
+    return block;
+  }
+
+  public int getNumber(){
+    return number;
+  }
+
+  public int getCapacity(){
+    return capacity;
+  }
+
+  public String toString(){
+    return "Room: " + block + floor + number + "\nCapacity: " + capacity + "\nAvailable: " + available;
   }
 }

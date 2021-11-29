49d16a0ac2a0643932a8a20452163b68ffbf2ad6
MartyBobo <martin.rosendahl4@gmail.com>
2021-11-29 09:14:01 +0100

    Update TimeInterval.java

--- Schedule/src/model/TimeInterval.java
+++ Schedule/src/model/TimeInterval.java
@@ -2,4 +2,21 @@ package model;
 
 public class TimeInterval
 {
+  private int hour;
+  private int minute;
+  private int numberOfLessons;
+  private Date date;
+
+  public TimeInterval(int hour, int minute, int numberOfLessons){
+    this.hour = hour;
+    this.minute = minute;
+    this.numberOfLessons = numberOfLessons;
+    date = new Date();
+  }
+
+  public boolean isOverlapped(TimeInterval other){
+    if (!(this.equals(other))){
+      return true;
+    }
+  }
 }

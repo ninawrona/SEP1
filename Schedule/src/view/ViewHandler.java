6a88e3624ababe487cd29e7e8227005dd45fc441
chrfoyer <chrfoyer@gmail.com>
2021-11-30 15:22:11 +0100

    Checking The Ammendments

--- Schedule/src/view/ViewHandler.java
+++ Schedule/src/view/ViewHandler.java
@@ -25,7 +25,7 @@ public class ViewHandler
   public void start(Stage primaryStage)
   {
     this.primaryStage = primaryStage;
-    openview("schedule");
+    //openview("schedule");
     addSessionViewController.reset();
     // More controllers here
   }

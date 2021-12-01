0f3f02eec18456f0dbc67dc7c009fa5c7e8636f8
ninawrona <90191093+ninawrona@users.noreply.github.com>
2021-12-01 12:31:17 +0100

    working with addSessionViewController

--- Schedule/src/view/AddSessionViewController.java
+++ Schedule/src/view/AddSessionViewController.java
@@ -1,13 +1,22 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 
-public class AddSessionViewController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddSessionViewController implements Initializable
 {
   //@FXML private methods here
-  @FXML Label errorLabel;
+  @FXML private Label errorLabel;
+  @FXML private ChoiceBox<> courseChoiceBoxInAddSession;
+  @FXML private ChoiceBox<> startTimeChoiceBox;
+  @FXML private ChoiceBox<> numberOfLessonsChoiceBox;
+  @FXML private ChoiceBox<> roomsChoiceBox;
   private Region root;
   private ViewHandler viewHandler;
   //private ScheduleModel model;
@@ -33,6 +42,16 @@ public class AddSessionViewController
   public void reset()
   {
     // set text to ""
+  }
+
+  //here we add our list of choices from an arrayList
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    courseChoiceBoxInAddSession.getItems().addAll(allCoursesArray);
+    startTimeChoiceBox.getItems().addAll(timeArray);
+    numberOfLessonsChoiceBox.getItems().addAll(numberOfLessonsArray); //int array I suppose
+    roomsChoiceBox.getItems().addAll(roomsArray); //rooms that are currently available, we have to change
+                                                  // the array after clicking find rooms
   }
 
   // @FXML methods here

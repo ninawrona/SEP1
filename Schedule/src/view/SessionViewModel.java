00d2309fe90e2d258ee53383e7625c4dfb6bb85a
chrfoyer <chrfoyer@gmail.com>
2021-12-02 14:20:41 +0100

    Update SessionViewModel properties
    These properties will allow the schedule to access certain properties of a session in question

--- Schedule/src/view/SessionViewModel.java
+++ Schedule/src/view/SessionViewModel.java
@@ -9,12 +9,18 @@ import java.time.LocalDate;
 public class SessionViewModel {
     private StringProperty courseProperty;
     private IntegerProperty dayOfWeekProperty;
+    private IntegerProperty startHourProperty;
+    private IntegerProperty startMinuteProperty;
+    private IntegerProperty numberOfLessonsProperty;
 
     public SessionViewModel(Session session) {
         courseProperty = new SimpleStringProperty(session.getCourse().getName());
         LocalDate localDate = LocalDate.of(session.getDate().getYear(), session.getDate().getMonth(), session.getDate().getDay());
         // Set day of week as an int. 1 is Monday, 7 is Sunday
         dayOfWeekProperty = new SimpleIntegerProperty(localDate.getDayOfWeek().getValue());
+        startHourProperty = new SimpleIntegerProperty(session.getStartTime().getHour());
+        startMinuteProperty = new SimpleIntegerProperty(session.getStartTime().getMinute());
+        numberOfLessonsProperty = new SimpleIntegerProperty(session.getNumberOfLessons());
     }
 
     public String getCourseProperty() {
@@ -23,5 +29,17 @@ public class SessionViewModel {
 
     public int getDayOfWeekProperty() {
         return dayOfWeekProperty.get();
+    }
+
+    public int getStartHourProperty() {
+        return startHourProperty.get();
+    }
+
+    public int getStartMinuteProperty() {
+        return startMinuteProperty.get();
+    }
+
+    public int getNumberOfLessonsProperty() {
+        return numberOfLessonsProperty.get();
     }
 }

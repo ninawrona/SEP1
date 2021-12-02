0c11a21acacdac5a6a01c1452f0e156655c29173
ninawrona <90191093+ninawrona@users.noreply.github.com>
2021-12-02 14:52:08 +0100

    Update ClassSelectViewController.java

--- Schedule/src/view/ClassSelectViewController.java
+++ Schedule/src/view/ClassSelectViewController.java
@@ -5,7 +5,13 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
+import model.basic.ClassGroup;
+import model.basic.Course;
 import model.list.ScheduleModel;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 public class ClassSelectViewController
 {
@@ -15,6 +21,8 @@ public class ClassSelectViewController
     private Region root;
     private ViewHandler viewHandler;
     private ScheduleModel model;
+
+    ArrayList<ClassGroup> allClassesArray = new ArrayList<>();
 
     public ClassSelectViewController()
     {
@@ -27,6 +35,21 @@ public class ClassSelectViewController
         this.root = root;
         this.model = model;
         reset();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadAllClassesArray();
+    }
+
+    private void loadAllClassesArray() {
+        // Clear the current arrayList
+        allClassesArray.removeAll(allClassesArray);
+
+        for (int i = 0; i < ScheduleViewModel.getClassGroup().size(); i++) {
+            allCoursesArray.add(model.getCourseListByClassGroup(classGroup).get(i));
+        }
+        courseChoiceBoxInAddSession.getItems().addAll(allCoursesArray);
     }
 
     public Region getRoot()

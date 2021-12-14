0da5408b7546fc159d43f78f971396426adbf924
Robert Barta <92293708+robert-barta@users.noreply.github.com>
2021-12-14 09:24:48 +0100

    simplify

--- Schedule/src/model/basic/Date.java
+++ Schedule/src/model/basic/Date.java
@@ -222,11 +222,7 @@ public class Date {
      * @return "True" when the year is a leap year. "False" when the year is not a leap year.
      */
     public boolean isLeapYear() {
-        if ((year % 4) == 0 && (((year % 100) != 0) || ((year % 400) == 0))) {
-            return true;
-        } else {
-            return false;
-        }
+        return (year % 4) == 0 && (((year % 100) != 0) || ((year % 400) == 0));
 
     }
 
